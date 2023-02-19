a93135a6079055dcac9edbb642ee4e14393d1d9f
Marcos Schumacher <marcosrschumacher@gmail.com>
2023-02-19 11:36:57 -0300

    definir fim do jogo

--- script.js
+++ script.js
@@ -44,6 +44,12 @@ function iniciarJogo() {
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Que pena, você perdeu! :(');
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -63,7 +69,6 @@ function iniciarJogo() {
     else{food.x = Math.floor(Math.random() * 15 + 1) * box;
          food.y = Math.floor(Math.random() * 15 + 1) * box;
     }
-
 
     let newHead = {
         x: snakeX,
